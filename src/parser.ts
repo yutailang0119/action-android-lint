c1b4766ca2db8b5ad5c8e6e4d2af3c3df6e0b909
yutailang0119 <muta.yutaro@gmail.com>
2020-04-11 02:20:55 +0900

    Fix parser for cosmetic

--- src/parser.ts
+++ src/parser.ts
@@ -2,38 +2,31 @@ import * as core from '@actions/core'
 import * as xml2js from 'xml2js'
 import {Message} from './message'
 
-const parseMessages = async (reportXml: string): Promise<Message[]> => {
+export async function parseXml(reportXml: string): Promise<Message[]> {
   const parser = new xml2js.Parser()
   const xml = await parser.parseStringPromise(reportXml)
-
-  const messages: Message[] = []
-  for (const issueElement of xml.issues.issue) {
-    const issue = issueElement.$
-
-    for (const locationElement of issueElement.location) {
-      const location = locationElement.$
-
-      const message = new Message(
-        issue.severity,
-        location.file,
-        parseInt(location.line),
-        parseInt(location.column),
-        `${issue.summary}: ${issue.message}`
-      )
-      core.debug(`Severity: ${message.severity}`)
-      core.debug(`Summary: ${message.description}`)
-      messages.push(message)
-    }
-  }
-
-  return messages
-}
-
-export async function parseXml(reportXml: string): Promise<Message[]> {
   return new Promise(resolve => {
     try {
-      const mesasges = parseMessages(reportXml)
-      resolve(mesasges)
+      const messages: Message[] = []
+      for (const issueElement of xml.issues.issue) {
+        const issue = issueElement.$
+
+        for (const locationElement of issueElement.location) {
+          const location = locationElement.$
+
+          const message = new Message(
+            issue.severity,
+            location.file,
+            parseInt(location.line),
+            parseInt(location.column),
+            `${issue.summary}: ${issue.message}`
+          )
+          core.debug(`Severity: ${message.severity}`)
+          core.debug(`Summary: ${message.description}`)
+          messages.push(message)
+        }
+      }
+      resolve(messages)
     } catch (error) {
       core.debug(`failed to read ${error}`)
     }
