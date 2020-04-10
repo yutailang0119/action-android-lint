5d48f4b619113226f4d87c5cf8a006a210eec761
yutailang0119 <muta.yutaro@gmail.com>
2020-04-10 20:51:21 +0900

    Fix main.tx to use command

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import * as core from '@actions/core'
+import {parse} from './parser'
+import {echoMessages} from './command'
 
 async function run(): Promise<void> {
   try {
     const reportXmlPath = core.getInput('reportXmlPath', {required: true})
-    core.debug(`Report xml path: ${reportXmlPath}`)
+    const messages = await parse(reportXmlPath)
+    await echoMessages(messages)
   } catch (error) {
     core.setFailed(error.message)
   }
