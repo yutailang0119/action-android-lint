4a07b69a3a03c30570af6f5b28be41aab72c0555
yutailang0119 <muta.yutaro@gmail.com>
2020-04-10 17:50:04 +0900

    Fix parser test

--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -2,7 +2,6 @@ import {parse} from '../src/parser'
 import * as path from 'path'
 
 test('test parse', () => {
-  // process.env['INPUT_REPORTXMLPATH'] = 'foo/bar/piyo'
   const filePath = path.join(__dirname, '..', 'lint-results.xml')
   parse(filePath)
 })
