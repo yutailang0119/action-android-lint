c416c6a883dc09328524c804f3c6f05aa6ceeb37
yutailang0119 <muta.yutaro@gmail.com>
2020-04-10 17:49:51 +0900

    Fix parser to use htmlparser2

--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,25 @@
 import * as core from '@actions/core'
+import {Parser} from 'htmlparser2'
 import fs from 'fs'
 
-export async function parse(reportXmlPath: string): Promise<[string]> {
+export async function parse(reportXmlPath: string): Promise<string[]> {
   return new Promise(resolve => {
     try {
-      const reportXml = fs.readFileSync(reportXmlPath)
-      core.debug(`${reportXml}`)
-      const buffer: [string] = ['']
+      const reportXml = fs.readFileSync(reportXmlPath, 'utf-8')
+      const buffer: string[] = []
+      const parser = new Parser(
+        {
+          ontext(text) {
+            core.debug(`--> ${text}`)
+          }
+        },
+        {
+          decodeEntities: false,
+          xmlMode: true
+        }
+      )
+      parser.write(reportXml)
+      parser.end()
       resolve(buffer)
     } catch (error) {
       core.debug(`failed to read ${error}`)
