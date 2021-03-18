a6df4e9153d67f94f0a4dd1d4dcb3097242d499e
yutailang0119 <yutailang0119@gmail.com>
2021-03-18 20:52:45 +0900

    npm run all

--- dist/index.js
+++ dist/index.js
@@ -47,15 +47,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.echoMessages = void 0;
 const command = __importStar(__nccwpck_require__(7351));
@@ -66,12 +57,10 @@ const commandProperties = (annotation) => {
         col: `${annotation.column}`
     };
 };
-function echoMessages(annotations) {
-    return __awaiter(this, void 0, void 0, function* () {
-        for (const annotation of annotations) {
-            command.issueCommand(annotation.severityLevel, commandProperties(annotation), annotation.message);
-        }
-    });
+async function echoMessages(annotations) {
+    for (const annotation of annotations) {
+        command.issueCommand(annotation.severityLevel, commandProperties(annotation), annotation.message);
+    }
 }
 exports.echoMessages = echoMessages;
 
@@ -102,22 +91,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __asyncValues = (this && this.__asyncValues) || function (o) {
-    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-    var m = o[Symbol.asyncIterator], i;
-    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
-    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
-    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -127,37 +100,24 @@ const glob = __importStar(__nccwpck_require__(8090));
 const fs_1 = __importDefault(__nccwpck_require__(5747));
 const parser_1 = __nccwpck_require__(267);
 const command_1 = __nccwpck_require__(524);
-function run() {
-    var e_1, _a;
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const xmlPath = core.getInput('xml_path', { required: true });
-            const globOptions = {
-                followSymbolicLinks: core.getInput('follow-symbolic-links').toUpperCase() !== 'FALSE'
-            };
-            const globber = yield glob.create(xmlPath, globOptions);
-            let annotations = [];
-            try {
-                for (var _b = __asyncValues(globber.globGenerator()), _c; _c = yield _b.next(), !_c.done;) {
-                    const file = _c.value;
-                    const xml = fs_1.default.readFileSync(file, 'utf-8');
-                    const annotation = yield parser_1.parseXml(xml);
-                    annotations = annotations.concat(annotation);
-                }
-            }
-            catch (e_1_1) { e_1 = { error: e_1_1 }; }
-            finally {
-                try {
-                    if (_c && !_c.done && (_a = _b.return)) yield _a.call(_b);
-                }
-                finally { if (e_1) throw e_1.error; }
-            }
-            yield command_1.echoMessages(annotations);
+async function run() {
+    try {
+        const xmlPath = core.getInput('xml_path', { required: true });
+        const globOptions = {
+            followSymbolicLinks: core.getInput('follow-symbolic-links').toUpperCase() !== 'FALSE'
+        };
+        const globber = await glob.create(xmlPath, globOptions);
+        let annotations = [];
+        for await (const file of globber.globGenerator()) {
+            const xml = fs_1.default.readFileSync(file, 'utf-8');
+            const annotation = await parser_1.parseXml(xml);
+            annotations = annotations.concat(annotation);
         }
-        catch (error) {
-            core.setFailed(error.message);
-        }
-    });
+        await command_1.echoMessages(annotations);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
 run();
 
@@ -188,41 +148,30 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.parseXml = void 0;
 const core = __importStar(__nccwpck_require__(2186));
 const xml2js = __importStar(__nccwpck_require__(6189));
 const Annotation_1 = __nccwpck_require__(7548);
-function parseXml(reportXml) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const parser = new xml2js.Parser();
-        const xml = yield parser.parseStringPromise(reportXml);
-        return new Promise(resolve => {
-            try {
-                const annotations = [];
-                for (const issueElement of xml.issues.issue) {
-                    const issue = issueElement.$;
-                    for (const locationElement of issueElement.location) {
-                        const location = locationElement.$;
-                        const annotation = new Annotation_1.Annotation(issue.severity, location.file, parseInt(location.line), parseInt(location.column), `${issue.summary}: ${issue.message}`);
-                        annotations.push(annotation);
-                    }
+async function parseXml(reportXml) {
+    const parser = new xml2js.Parser();
+    const xml = await parser.parseStringPromise(reportXml);
+    return new Promise(resolve => {
+        try {
+            const annotations = [];
+            for (const issueElement of xml.issues.issue) {
+                const issue = issueElement.$;
+                for (const locationElement of issueElement.location) {
+                    const location = locationElement.$;
+                    const annotation = new Annotation_1.Annotation(issue.severity, location.file, parseInt(location.line), parseInt(location.column), `${issue.summary}: ${issue.message}`);
+                    annotations.push(annotation);
                 }
-                resolve(annotations);
             }
-            catch (error) {
-                core.debug(`failed to read ${error}`);
-            }
-        });
+            resolve(annotations);
+        }
+        catch (error) {
+            core.debug(`failed to read ${error}`);
+        }
     });
 }
 exports.parseXml = parseXml;
