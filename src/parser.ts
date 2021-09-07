e0f09ed1ded5d4cddce5c6fde6698ac9cb33cb60
yutailang0119 <yutailang0119@gmail.com>
2021-09-08 00:16:55 +0900

    Rename file of Annotation

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 import * as xml2js from 'xml2js'
-import {Annotation} from './Annotation'
+import {Annotation} from './annotation'
 
 export async function parseXml(reportXml: string): Promise<Annotation[]> {
   const parser = new xml2js.Parser()
