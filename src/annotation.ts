ae62509e4e7b7fa4e055e91b3564f78d585415cb
yutailang0119 <yutailang0119@gmail.com>
2021-10-04 00:40:30 +0900

    Fix to use @actions/core/AnnotationProperties

--- src/annotation.ts
+++ src/annotation.ts
@@ -1,15 +1,24 @@
+import {AnnotationProperties} from '@actions/core'
 import {AnnotationSeverityLevel} from './annotation-severity-level'
 
 export class Annotation {
   severityLevel: AnnotationSeverityLevel
+  error: Error
+  properties: AnnotationProperties
 
   constructor(
     severity: string,
-    public message: string,
-    public file: string,
-    public line: number,
-    public column: number
+    message: string,
+    file: string,
+    line: number,
+    column: number
   ) {
     this.severityLevel = severity === 'Error' ? 'error' : 'warning'
+    this.error = new Error(message)
+    this.properties = {
+      file,
+      startLine: line,
+      startColumn: column
+    }
   }
 }
