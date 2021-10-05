bb65d56f1dd6cd751f481c8541446ad8464c9cfb
yutailang0119 <yutailang0119@gmail.com>
2021-10-06 00:14:43 +0900

    Fix command to change output

--- src/command.ts
+++ src/command.ts
@@ -4,9 +4,9 @@ import {Annotation} from './annotation'
 export const echoMessages = (annotations: Annotation[]): void => {
   for (const annotation of annotations) {
     if (annotation.severityLevel === 'error') {
-      core.error(annotation.error, annotation.properties)
+      core.error(annotation.error.message, annotation.properties)
     } else {
-      core.warning(annotation.error, annotation.properties)
+      core.warning(annotation.error.message, annotation.properties)
     }
   }
 }
