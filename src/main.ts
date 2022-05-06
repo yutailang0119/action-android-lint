e3f268fcc5fc618dfb09e9b41da74c1cbce3af86
Ryan <rbis@hubbell.com>
2022-05-06 09:54:28 -0400

    try again

--- src/main.ts
+++ src/main.ts
@@ -64,7 +64,7 @@ class LintReporter {
         output: {
           title: `Some title, yo`,
           summary,
-          annotations: null
+          annotations: []
         },
         ...github.context.repo
       })
