07fa87008a855dd308e0c7c8fd7b9f16c05491cd
Ryan <rbis@hubbell.com>
2022-05-06 09:36:38 -0400

    built new version

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import * as core from '@actions/core'
 import * as glob from '@actions/glob'
 import * as github from '@actions/github'
-import {echoMessages} from './command'
-import {parseLintXmls, parseXmls} from './parser'
+// import {echoMessages} from './command'
+import {parseLintXmls /*, parseXmls*/} from './parser'
 import {GitHub} from '@actions/github/lib/utils'
-import {getCheckRunContext} from './utils/github-utils'
+// import {getCheckRunContext} from './utils/github-utils'
+import {EventPayloads} from '@octokit/webhooks'
 
 async function main(): Promise<void> {
   try {
@@ -18,7 +19,7 @@ async function main(): Promise<void> {
 class LintReporter {
   readonly token = core.getInput('token', {required: true})
   readonly octokit: InstanceType<typeof GitHub>
-  readonly context = getCheckRunContext()
+  readonly context = this.getCheckRunContext()
 
   constructor() {
     this.octokit = github.getOctokit(this.token)
@@ -54,7 +55,7 @@ class LintReporter {
       const lintIssues = await parseLintXmls(files)
       const conclusion = 'success'
 
-      const resp = this.octokit.checks.update({
+      const resp = await this.octokit.checks.update({
         check_run_id: createResp.data.id,
         conclusion,
         status: 'completed',
@@ -82,6 +83,37 @@ class LintReporter {
     } catch (error) {
       if (error instanceof Error) core.setFailed(error.message)
     }
+  }
+
+  getCheckRunContext(): {sha: string; runId: number} {
+    if (github.context.eventName === 'workflow_run') {
+      core.info(
+        'Action was triggered by workflow_run: using SHA and RUN_ID from triggering workflow'
+      )
+      const event = github.context
+        .payload as EventPayloads.WebhookPayloadWorkflowRun
+      if (!event.workflow_run) {
+        throw new Error(
+          "Event of type 'workflow_run' is missing 'workflow_run' field"
+        )
+      }
+      return {
+        sha: event.workflow_run.head_commit.id,
+        runId: event.workflow_run.id
+      }
+    }
+
+    const runId = github.context.runId
+    if (github.context.payload.pull_request) {
+      core.info(
+        `Action was triggered by ${github.context.eventName}: using SHA from head of source branch`
+      )
+      const pr = github.context.payload
+        .pull_request as EventPayloads.WebhookPayloadPullRequestPullRequest
+      return {sha: pr.head.sha, runId}
+    }
+
+    return {sha: github.context.sha, runId}
   }
 }
 
